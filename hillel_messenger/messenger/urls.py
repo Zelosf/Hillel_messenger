901b89605553f3c9fe6084085e36bb474085eb1f
Zelosf <Zelosf@gmail.com>
2024-07-15 19:36:57 +0300

    Add 10 mixins

--- hillel_messenger/messenger/urls.py
+++ hillel_messenger/messenger/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.chat_list, name='chat_list'),
-    path('chat/<int:chat_id>/', views.chat_detail, name='chat_detail'),
-    path('chat/<int:chat_id>/send_message/', views.send_message, name='send_message'),
-    path('message/<int:message_id>/edit/', views.edit_message, name='edit_message'),
-    path('message/<int:message_id>/delete/', views.delete_message, name='delete_message'),
-    path('chat/<int:chat_id>/users/status/list/', views.user_status_list, name='user_status_list'),
+    path('', views.chat_list.as_view(), name='chat_list'),
+    path('chat/<int:chat_id>/', views.chat_detail.as_view(), name='chat_detail'),
+    path('chat/<int:chat_id>/send_message/', views.send_message.as_view(), name='send_message'),
+    path('message/<int:message_id>/edit/', views.edit_message.as_view(), name='edit_message'),
+    path('message/<int:message_id>/delete/', views.delete_message.as_view(), name='delete_message'),
+    path('chat/<int:chat_id>/users/status/list/', views.user_status_list.as_view(), name='user_status_list'),
 ]
--- hillel_messenger/messenger/views.py
+++ hillel_messenger/messenger/views.py
@@ -1,99 +1,75 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required, permission_required
-from .models import Chat, Message, MessageLog, UserStatus
-from django.contrib.auth.models import User
+from .models import Chat, Message, UserStatus
 from django.http import HttpResponseForbidden, Http404, JsonResponse
 from django.contrib import messages
+from django.views import View
+from .mixins import (
+    CustomLoginRequiredMixin,
+    CustomPermissionRequiredMixin,
+    UserIsParticipantMixin,
+    JsonResponseMixin,
+    FormValidMessageMixin,
+    ObjectOwnerMixin
+)
 
+class chat_list(CustomLoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        chats = Chat.objects.filter(participants=request.user)
+        return render(request, 'chats.html', {'chats': chats})
 
-
-
-@login_required
-def chat_list(request):
-    chats = Chat.objects.filter(participants=request.user)
-    return render(request, 'chats.html', {'chats': chats})
-
-
-
-@login_required
-def chat_detail(request, chat_id):
-    try:
+class chat_detail(CustomLoginRequiredMixin, UserIsParticipantMixin, View):
+    def get(self, request, chat_id, *args, **kwargs):
         chat = Chat.objects.get(id=chat_id)
-    except Chat.DoesNotExist:
-        raise Http404("Chat not found")
-    if request.user not in chat.participants.all():
-        return HttpResponseForbidden()
-    messages = chat.messages.all()
-    return render(request, 'chat.html', {'chat': chat, 'chatmessages': messages})
+        messages = chat.messages.all()
+        return render(request, 'chat.html', {'chat': chat, 'chatmessages': messages})
 
+class send_message(CustomLoginRequiredMixin, UserIsParticipantMixin, FormValidMessageMixin, View):
+    success_message = "Message sent successfully."
 
-
-@login_required
-def send_message(request, chat_id):
-    try:
+    def post(self, request, chat_id, *args, **kwargs):
         chat = Chat.objects.get(id=chat_id)
-    except Chat.DoesNotExist:
-        raise Http404("Chat not found")
-    if request.user not in chat.participants.all():
-        return HttpResponseForbidden()
-    if request.method == 'POST':
         content = request.POST.get('content')
         if content:
             Message.objects.create(chat=chat, author=request.user, content=content)
             if chat.participants.count() == 2:
                 other_user = chat.participants.exclude(id=request.user.id).first()
                 if other_user.is_superuser:
-                    messages.success(request, 'Ви успішно надіслали повідомлення суперюзеру')
-    return redirect('chat_detail', chat_id=chat.id)
+                    messages.success(request, '!!!Ви успішно надіслали повідомлення суперюзеру!!!')
+        return redirect('chat_detail', chat_id=chat.id)
 
+class edit_message(CustomLoginRequiredMixin, CustomPermissionRequiredMixin, ObjectOwnerMixin, View):
+    permission_required = 'messenger.can_edit_own_message'
 
-@login_required
-@permission_required('messenger.can_edit_own_message')
-def edit_message(request, message_id):
-    try:
+    def get(self, request, message_id, *args, **kwargs):
         message = Message.objects.get(id=message_id)
-    except Message.DoesNotExist:
-        raise Http404("Message not found")
-    if request.user != message.author:
-        return HttpResponseForbidden()
-    if request.method == 'POST':
+        return render(request, 'edit_message.html', {'message': message})
+
+    def post(self, request, message_id, *args, **kwargs):
+        message = Message.objects.get(id=message_id)
         content = request.POST.get('content')
         if content:
             message.content = content
             message.save()
             return redirect('chat_detail', chat_id=message.chat.id)
-    return render(request, 'edit_message.html', {'message': message})
+        return render(request, 'edit_message.html', {'message': message})
 
+class delete_message(CustomLoginRequiredMixin, CustomPermissionRequiredMixin, ObjectOwnerMixin, View):
+    permission_required = 'messenger.can_delete_own_message'
 
-@login_required
-@permission_required('messenger.can_delete_own_message')
-def delete_message(request, message_id):
-    try:
+    def post(self, request, message_id, *args, **kwargs):
         message = Message.objects.get(id=message_id)
-    except Message.DoesNotExist:
-        raise Http404("Message not found")
-    if request.user != message.author:
-        return HttpResponseForbidden()
-    chat_id = message.chat.id
-    message.delete()
-    return redirect('chat_detail', chat_id=chat_id)
+        chat_id = message.chat.id
+        message.delete()
+        return redirect('chat_detail', chat_id=chat_id)
 
-
-@login_required
-def user_status_list(request, chat_id):
-    try:
+class user_status_list(CustomLoginRequiredMixin, UserIsParticipantMixin, JsonResponseMixin, View):
+    def get(self, request, chat_id, *args, **kwargs):
         chat = Chat.objects.get(id=chat_id)
-    except Chat.DoesNotExist:
-        raise Http404("Chat not found")
-    if request.user not in chat.participants.all():
-        return HttpResponseForbidden()
-
-    users_data = []
-    for participant in chat.participants.all():
-        try:
-            user_status = UserStatus.objects.get(user=participant)
-            users_data.append({'name': participant.username, 'is_online': user_status.is_online})
-        except UserStatus.DoesNotExist:
-            users_data.append({'name': participant.username, 'is_online': False})
-
-    return JsonResponse(users_data, safe=False)
+        users_data = []
+        for participant in chat.participants.all():
+            try:
+                user_status = UserStatus.objects.get(user=participant)
+                users_data.append({'name': participant.username, 'is_online': user_status.is_online})
+            except UserStatus.DoesNotExist:
+                users_data.append({'name': participant.username, 'is_online': False})
+        return self.render_to_json_response(users_data)
